b7b88c1993278351617c38e5f585f6d306730548
GarhiSDGs_Diamond <sdgs.diamond@garhijapan.com>
2025-12-04 15:35:47 +0900

    2025/12/04 15:35 - 1st Commit

---
+++ Assets/Scripts/ActorSO/AgeGroupDataSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "世代データ", menuName = "Scriptable Objects/AgeGroupDataSO")]
+public class AgeGroupDataSO : ScriptableObject {
+    public string ageGroupName; // 高校生、小学生
+    public int minHeight; // 高校生なら130
+    public int maxHeight; // 高校生なら179
+    public List<TeamDataSO> selectableTeams; // 属するチームリスト（レッドスター、ブルーなど）
+}
---
+++ Assets/Scripts/ActorSO/TeamDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "チームデータ", menuName = "Scriptable Objects/TeamDataSO")]
+public class TeamDataSO : ScriptableObject {
+    public string teamName;
+    public Sprite teamLogo;
+    public int difficultyStars;
+    public string description; // ゲーム内の説明文
+    // ...このチーム専用の練習効率定数など
+}
